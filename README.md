c500dabd2eefa6e517b3262d917282fc58b9d709
Alan Astroga Hurtado <alanasto10@gmail.com>
2022-10-18 12:16:57 -0500

    fin modelos para put, post ...

---
+++ README.md
@@ -0,0 +1,7 @@
+Para correr localmente, se necesita la base de datos local 
+### no olvidar generar el archivo docker-compose
+
+```
+docker-compose up -d
+```
+mongodb://localhost:27017/entriesdb
--- models/server.js
+++ models/server.js
@@ -1,35 +1,56 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('../database/config');
+
 class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT || 3000;
-    this.usersPath = "/api/users";
 
-    //Middlewares
-    this.middlewares();
-    //Rutas de mi aplicación
-    this.routes();
-  }
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
-  middlewares() {
-    // CORS
-    this.app.use(cors());
-    //lectura y parseo del body
-    this.app.use(express.json());
-    //Directorio publico
-    this.app.use(express.static("public"));
-  }
+        // Conectar a base de datos
+        this.conectarDB();
 
-  routes() {
-    this.app.use(this.usersPath, require("../routes/user"));
-  }
+        // Middlewares
+        this.middlewares();
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log(`Example app listening on port ${this.port}`);
-    });
-  }
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
 }
+
+
+
 
 module.exports = Server;
